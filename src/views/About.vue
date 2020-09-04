506c684a9e138a1bff12f2c2eeecdb0c89043ecc
kimsam0 <ezrinn@hanmail.net>
2020-09-04 13:40:01 +0900

    레이아웃 test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>소개</h1>
+    <div>소개</div>
   </div>
 </template>
--- src/views/Auto.vue
+++ src/views/Auto.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="auto">
     <h1>auto</h1>
+    <div>
+      <b-tabs content-class="mt-3">
+        <b-tab title="First" active><p>I'm the first tab</p></b-tab>
+        <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+        <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+      </b-tabs>
+    </div>
   </div>
 </template>
